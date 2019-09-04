c8a2af793a8609399cfb5dcc7564849c5074b80a
wswind <18399096+wswind@users.noreply.github.com>
2019-09-04 13:57:10 +0800

    Update IIncludable .cs

--- EfcoreIncludeDemo/EfcoreIncludeDemo/ForIncludes/IIncludable .cs
+++ EfcoreIncludeDemo/EfcoreIncludeDemo/ForIncludes/IIncludable .cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EfcoreIncludeDemo.Application
+namespace EfcoreIncludeDemo.ForIncludes
 {
     public interface IIncludable { }
 
